81cdb0fcf3ad57af63433bf039d35b2eaccbae41
Francisco Meireles <franciscomeireles-10@hotmail.com>
2021-03-30 08:53:31 +0100

    - Server gera chave de euromilhões para cada pedido, evitando a repetição de números dentro da mesma chave

--- Trabalho_1/Server/mtserver.c
+++ Trabalho_1/Server/mtserver.c
@@ -8,19 +8,30 @@ Simple winsock Server
 #include <time.h>
 #define TRUE 1
 #define DS_TEST_PORT 68000
+#define MAX_NUMBER 50
+#define MIN_NUMBER 1
+#define MAX_STAR 12
+#define MIN_STAR 1
 
 #pragma comment (lib, "ws2_32.lib")
 #pragma warning(disable : 4996)
-
 
 // function ot handle the incoming connection
 //	param: the socket of the calling client
 
 DWORD WINAPI handleconnection(LPVOID lpParam);
-
+int* random_key(int (*random_number)(int, int));
+int random_number(int min_num, int max_num);
+void save_key_to_file(int* key);
+char* convert_array_to_string(int array[], int n);
+int* check_duplicates(int* array, int* size, int lower_bound, int upper_bound);
+void print_array(int* a, int len);
+int find_elem(int value, const int a[], int m, int n);
 
 int main()
 {
+	srand(time(NULL));
+
 	// Initialise winsock
 	WSADATA wsData;
 	WORD ver = MAKEWORD(2, 2);
@@ -47,7 +58,7 @@ int main()
 	hint.sin_port = htons(DS_TEST_PORT);
 	hint.sin_addr.S_un.S_addr = INADDR_ANY;
 
-	bind(listening, (struct sockaddr*) & hint, sizeof(hint));
+	bind(listening, (struct sockaddr*)&hint, sizeof(hint));
 	printf("\nSocket binded.");
 
 	// Setup the socket for listening
@@ -61,53 +72,49 @@ int main()
 	SOCKET* ptclientSocket;
 	DWORD dwThreadId;
 	HANDLE  hThread;
-	int conresult=0;
+	int conresult = 0;
 
-	while (TRUE) 
+	while (TRUE)
 	{
 		clientSize = sizeof(client);
-		clientSocket = accept(listening, (struct sockaddr*) & client, &clientSize);
+		clientSocket = accept(listening, (struct sockaddr*)&client, &clientSize);
 
 		ptclientSocket = &clientSocket;
 
 		printf("\nHandling a new connection.");
 
-		// Handle the communication with the client 
+		// Handle the communication with the client
 
 		hThread = CreateThread(
 			NULL,                   // default security attributes
-			0,                      // use default stack size  
+			0,                      // use default stack size
 			handleconnection,       // thread function name
-			ptclientSocket,          // argument to thread function 
-			0,                      // use default creation flags 
-			&dwThreadId);   // returns the thread identifier 
-
+			ptclientSocket,          // argument to thread function
+			0,                      // use default creation flags
+			&dwThreadId);   // returns the thread identifier
 
 		// Check the return value for success.
-		// If CreateThread fails, terminate execution. 
-		
+		// If CreateThread fails, terminate execution.
+
 		if (hThread == NULL)
 		{
 			printf("\nThread Creation error.");
 			ExitProcess(3);
 		}
-	} 
+	}
 
 	// Close the socket
 	closesocket(clientSocket);
 
 	// Close listening socket
 	closesocket(listening);
-	
+
 	//Cleanup winsock
 	WSACleanup();
 }
 
-
-
 DWORD WINAPI handleconnection(LPVOID lpParam)
 {
-
 	char strMsg[1024];
 	char strRec[1024];
 
@@ -115,7 +122,7 @@ DWORD WINAPI handleconnection(LPVOID lpParam)
 	SOCKET cs;
 	SOCKET* ptCs;
 
-	ptCs = (SOCKET*) lpParam;
+	ptCs = (SOCKET*)lpParam;
 	cs = *ptCs;
 
 	strcpy(strMsg, "\nHello! welcome to the server...\n");
@@ -135,7 +142,7 @@ DWORD WINAPI handleconnection(LPVOID lpParam)
 		}
 
 		printf("%i : %s\n", i++, strRec);
-		send(cs, strRec, bytesReceived + 1, 0);
+		//send(cs, strRec, bytesReceived + 1, 0);
 
 		if (strcmp(strRec, "date") == 0) {
 			// current date/time based on current system
@@ -152,9 +159,52 @@ DWORD WINAPI handleconnection(LPVOID lpParam)
 			// just to echo!
 			// send(cs, strRec, bytesReceived + 1, 0);
 		}
+		else if (strcmp(strRec, "get") == 0) {
+			// Gerar a Chave
 
-		if (strcmp(strRec, "bye") == 0) {
+			int* key_generated = random_key(random_number);
 
+			if (!key_generated) {
+				printf("ERROR: Não foi possível gerar a chave do euromilhões");
+			}
+
+			// Verificar pela existência de duplicações na chave gerada
+			int size_numbers_duplicates = 0;
+			int size_stars_duplicates = 0;
+			int* numbers_duplicated = check_duplicates(key_generated, &size_numbers_duplicates, 0, 4);
+			int* stars_duplicated = check_duplicates(key_generated, &size_stars_duplicates, 5, 7);
+
+			if (size_numbers_duplicates != 0 || size_stars_duplicates != 0) {
+				printf("ERROR: Chave possui números repetidos");
+			}
+
+			print_array(key_generated, 7);
+
+			// Abrir o ficheiro para efetuar o registo
+			//save_key_to_file(key_generated);
+
+			// Enviar para o cliente a chave gerada
+
+			strcpy(strMsg, "\n\nChave do euromilhões: ");
+			strcpy(strMsg, convert_array_to_string(key_generated, 7));
+
+			//strcat(strMsg, convert_array_to_string(int array[], int n));
+			strcat(strMsg, "\n");
+
+			send(cs, strMsg, strlen(strMsg) + 1, 0);
+
+			// Libertar memória
+			free(key_generated);
+			key_generated = NULL;
+		}
+		else if (strcmp(strRec, "help") == 0)
+		{
+			strcpy(strMsg, "\n\nHelp: ");
+			strcat(strMsg, "\n");
+
+			send(cs, strMsg, strlen(strMsg) + 1, 0);
+		}
+		else if (strcmp(strRec, "quit") == 0) {
 			strcpy(strMsg, "\nBye client...\n");
 			send(cs, strMsg, strlen(strMsg) + 1, 0);
 
@@ -162,7 +212,135 @@ DWORD WINAPI handleconnection(LPVOID lpParam)
 			closesocket(cs);
 			return 0;
 		}
-
 	}
-	
+}
+// Função que gera uma chave do euromilhoes de forma aleatória
+int* random_key(int (*random_number)(int, int)) {
+	// Array que vai armazenar a chave do euromilhoes
+	int* key = (int*)malloc(7 * sizeof(int));
+	if (!key)
+		return NULL;
+
+	int i = 0;
+	int temp = 0;
+	// Gerar os 5 números
+	for (i = 0; i < 5; i++)
+	{
+	generate_number:
+		temp = (*random_number)(MIN_NUMBER, MAX_NUMBER);
+		// O elemento gerado encontra-se no array, temos que evitar situações de duplicações
+
+		if (find_elem(temp, key, 0, i) != -1)
+		{
+			goto generate_number;
+		}
+		// O elemento gerado não se encontra no array
+		else {
+			key[i] = temp;
+		}
+	}
+
+	// Gerar as estrelas
+	for (; i < 7; i++)
+	{
+	generate_star:
+		temp = (*random_number)(MIN_STAR, MAX_STAR);
+		// O elemento gerado encontra-se no array, temos que evitar situações de duplicações
+		if (find_elem(temp, key, 5, i) != -1)
+		{
+			goto generate_star;
+		}
+		// O elemento gerado não se encontra no array
+		else {
+			key[i] = temp;
+		}
+	}
+
+	// Retorna a chave
+	return key;
+}
+
+/*Retorna um random número inteiro, no intervalo [min_num, max_num]*/
+int random_number(int min_num, int max_num)
+{
+	int result = 0, low_num = 0, hi_num = 0;
+
+	if (min_num < max_num)
+	{
+		low_num = min_num;
+		hi_num = max_num + 1; // include max_num in output
+	}
+	else {
+		low_num = max_num + 1; // include max_num in output
+		hi_num = min_num;
+	}
+
+	//srand(time(NULL));
+	result = (rand() % (hi_num - low_num)) + low_num;
+	return result;
+}
+
+void save_key_to_file(int* key) {
+	FILE* fp;
+	if ((fp = fopen("test.txt", "wb")) == NULL) {
+		printf("Não foi possível abrir o ficheiro.\n");
+	}
+
+	// Regista o ficheiro, a chave gerada
+	if (fwrite(key, sizeof(int), 7, fp) != 7)
+		printf("File read error.");
+
+	// Fecha ficheiro
+	fclose(fp);
+}
+
+// Converte um array de inteiros para uma string
+char* convert_array_to_string(int array[], int n) {
+	int i;
+	char* output = (char*)malloc(128);
+	char* point = output + 1;
+	*output = '[';
+	for (i = 0; i != n; ++i)
+		point += sprintf(point, i + 1 != n ? "%d," : "%d]", array[i]);
+
+	return output;
+}
+
+// A função procura elementos duplicados dentro de um array
+// lower_bound e upper_bound define os limites de procura dentro do array
+int* check_duplicates(int* array, int* size, int lower_bound, int upper_bound)
+{
+	// Elementos Duplicados
+	int* duplicates = (int*)malloc(7 * sizeof(int));
+	if (duplicates == NULL)
+		return NULL;
+
+	*size = 0;
+	// Número de elementos a avaliar
+	int count = (upper_bound - lower_bound + 1);
+
+	for (int i = lower_bound; i < lower_bound + count - 1; i++) { // read comment by @nbro
+		for (int j = i + 1; j < lower_bound + count; j++) {
+			if (array[i] == array[j]) {
+				printf("%d[%d] ==  %d[%d]\n", array[i], i, array[j], j);
+				duplicates[*size] = array[i];
+				*size += 1;
+			}
+		}
+	}
+
+	return duplicates;
+}
+
+// Imprime array para efeitos de debug
+void print_array(int* a, int len) {
+	for (int i = 0; i < len; i++) printf("%d ", a[i]);
+}
+
+// Procura elemento no array
+int find_elem(int value, const int a[], int m, int n)
+{
+	while (m < n && a[m] != value) m++;
+
+	return m == n ? -1 : m;
 }
